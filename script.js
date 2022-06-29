2213c6b37ded6f318e7b045c797a9ee48059ef92
benlaird7 <benjaminoliverlaird@gmail.com>
2022-06-29 20:48:18 +0100

    added function to create outcomes

--- script.js
+++ script.js
@@ -3,3 +3,26 @@ function computerPlay() {
     return computerSelect[Math.floor(Math.random() * 3)];
 }
 
+const playRound = (playerSelect, computerSelect) => {
+    if(playerSelect == 'Rock' && computerSelect === 'Scissors') {
+        return "You Win!";
+    }
+    else if(playerSelect == 'Scissors' && computerSelect === 'Paper') {
+        return "You Win!";
+    }
+    else if(playerSelect == 'Paper' && computerSelect === 'Scissors') {
+        return "You Win!";
+    }
+    else if(playerSelect == 'Rock' && computerSelect ==='Paper') {
+        return "You Lose :(";
+    }
+    else if(playerSelect == 'Scissors' && computerSelect ==='Rock') {
+        return "You Lose :(";
+    }
+    else if(playerSelect == 'Paper' && computerSelect ==='Scissors') {
+        return "You Lose :(";
+    }
+    else {
+        return "Draw";
+    }
+}
