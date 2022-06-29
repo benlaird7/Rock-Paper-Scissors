7bcc6777102697bcbad012829ea94e521388c476
benlaird7 <benjaminoliverlaird@gmail.com>
2022-06-29 22:51:23 +0100

    removed score function as wasnt working

--- script.js
+++ script.js
@@ -7,71 +7,37 @@ let computerSelect = computerPlay();
 
 const playRound = (playerSelect, computerSelect) => {
     //Player Chooses Rock//
-    if ((playerSelect.toUpperCase() === "ROCK") && (computerSelect === "Paper")) {
+    if ((playerSelect === "ROCK") && (computerSelect === "Paper")) {
         return "You Lose :(";
-      } else if ((playerSelect.toUpperCase() === "ROCK") && (computerSelect === "Rock")) {
+    } else if ((playerSelect === "ROCK") && (computerSelect === "Rock")) {
         return "It's a Draw!";
-      } else if ((playerSelect.toUpperCase() === "ROCK") && (computerSelect === "Scissors")) {
+    } else if ((playerSelect === "ROCK") && (computerSelect === "Scissors")) {
         return "You Win!";
-      }
-      //player chooses scissors//
-      if ((playerSelect.toUpperCase() === "SCISSORS") && (computerSelect === "Rock")) {
-        return "You Lose :(";
-      } else if ((playerSelect.toUpperCase() === "SCISSORS") && (computerSelect === "Scissors")) {
-        return "It's a Draw!";
-      } else if ((playerSelect.toUpperCase() === "SCISSORS") && (computerSelect === "Paper")) {
-        return "You Win :D";
-      }
-      //player chooses paper//
-      if ((playerSelect.toUpperCase() === "PAPER") && (computerSelect === "Scissors")) {
-        return "You Lose :(";
-      } else if ((playerSelect.toUpperCase() === "PAPER") && (computerSelect === "Paper")) {
-        return "It's a Draw!";
-      } else if ((playerSelect.toUpperCase() === "PAPER") && (computerSelect === "Rock")) {
-        return "You Win :D";
-      }
     }
-
-
-const game = () => {
-    for(i = 0; i < 5; i++);
-    playRound();
+    //player chooses scissors//
+    if ((playerSelect === "SCISSORS") && (computerSelect === "Rock")) {
+        return "You Lose :(";
+    } else if ((playerSelect === "SCISSORS") && (computerSelect === "Scissors")) {
+        return "It's a Draw!";
+    } else if ((playerSelect === "SCISSORS") && (computerSelect === "Paper")) {
+        return "You Win :D";
+    }
+    //player chooses paper//
+    if ((playerSelect === "PAPER") && (computerSelect === "Scissors")) {
+        return "You Lose :(";
+    } else if ((playerSelect === "PAPER") && (computerSelect === "Paper")) {
+        return "It's a Draw!";
+    } else if ((playerSelect === "PAPER") && (computerSelect === "Rock")) {
+        return "You Win :D";
+    }
 }
 
-const Scoreboard= () => {
-    let playerScore = 0;
-    let x = playerScore;
-    let computerScore = 0;
-    let y = computerScore;
-    //Player Chooses Rock//
-    if ((playerSelect.toUpperCase() === "ROCK") && (computerSelect === "Paper")) {
-        return(Number(x), Number(y) + 1);
-      } else if ((playerSelect.toUpperCase() === "ROCK") && (computerSelect === "Rock")) {
-        return (Number(x), Number(y));
-      } else if ((playerSelect.toUpperCase() === "ROCK") && (computerSelect === "Scissors")) {
-        return (Number(x) + 1, Number(y));
-      }
-      //player chooses scissors//
-      if ((playerSelect.toUpperCase() === "SCISSORS") && (computerSelect === "Rock")) {
-        return(Number(x), Number(y) + 1);
-      } else if ((playerSelect.toUpperCase() === "SCISSORS") && (computerSelect === "Scissors")) {
-        return (Number(x), Number(y));
-      } else if ((playerSelect.toUpperCase() === "SCISSORS") && (computerSelect === "Paper")) {
-        return (Number(x) + 1, Number(y));
-      }
-      //player chooses paper//
-      if ((playerSelect.toUpperCase() === "PAPER") && (computerSelect === "Scissors")) {
-        return (Number(x), Number(y) + 1);
-      } else if ((playerSelect.toUpperCase() === "PAPER") && (computerSelect === "Paper")) {
-        return (Number(x), Number(y));
-      } else if ((playerSelect.toUpperCase() === "PAPER") && (computerSelect === "Rock")) {
-        return (Number(x) + 1, Number(y));
-      }
-      if (x > y) {
-        return "YOU WIN!!"
-      }
-      else if (x < y) {
-        return "You lose";
-      }
+
+function game() {
+    for(let i = 0; i < 5; i++) {
+        playRound();
     }
+}
+    
+game()
 
