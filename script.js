e2c1c973f0a9af8e920892408f3d5c6be381951e
benlaird7 <benjaminoliverlaird@gmail.com>
2022-07-03 15:29:43 +0100

    added scoring system and game function which loops for 5

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let rock = 'Rock';
 let paper = 'Paper';
 let scissors = 'Scissors';
+let playerScore = 0;
+let computerScore = 0;
 rock > scissors;
 scissors > paper;
 paper > rock;
@@ -9,26 +11,38 @@ function computerPlay() {
     const computerSelect = ['ROCK', 'PAPER', 'SCISSORS'];
     return computerSelect[Math.floor(Math.random() * computerSelect.length)];
 }
-let playerSelect = prompt("Choose rock, paper, or scissors");
-let computerSelect = computerPlay();
 
-function round(playerSelect, computerSelect) {
+function playRound(playerSelect, computerSelect) {
 
-    if(playerSelect.toUpperCase() > computerSelect) {
-        return "You Win!" && alert("You Win!");
+    if(playerSelect > computerSelect) {
+        playerScore++;
+        return "You Win!";
     }
-    else if(playerSelect.toUpperCase() < computerSelect) {
-        return "You Lose" && alert("You Lose");
+    else if(playerSelect < computerSelect) {
+        computerScore++;
+        return "You Lose";
     }
-    else if(playerSelect.toUpperCase() === computerSelect) {
-        return "Draw" && alert("It's a Draw");
+    else if(playerSelect === computerSelect) {
+        return "Draw";
     }
 }
 
 
 function game() {
-    for(let i = 0; i < 5; i++); {
-        round(playerSelect, computerSelect);
+    for(let i = 0; i < 5; i++) {
+        let playerSelect = prompt('Choose rock, paper, or scissors!');
+        playerSelect = playerSelect.toUpperCase();
+        let computerSelect = computerPlay();
+        console.log(playRound(playerSelect, computerSelect));
+    }
+    if(playerScore > computerScore) {
+        console.log(`You're the winner! Final Score: ${playerScore} to ${computerScore}`);
+    }
+    else if(playerScore < computerScore) {
+        console.log(`You Lost :( Final Score: ${playerScore} to ${computerScore}`);
+    }
+    else {
+        console.log(`It's a draw! Final Score: ${playerScore} to ${computerScore}`);
     }
     
 }
